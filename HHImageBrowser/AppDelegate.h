88f129575d582fa508b146ecae3f3725192d294d
isbase <4656587@qq.com>
2015-01-12 18:53:26 +0800

    add
    add new project

---
+++ HHImageBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHImageBrowser
+//
+//  Created by DEMAI on 15-1-12.
+//  Copyright (c) 2015年 Today. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHImageBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHImageBrowser
+//
+//  Created by DEMAI on 15-1-12.
+//  Copyright (c) 2015年 Today. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
